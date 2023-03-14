063311b6dbfb92d37555a17baea0769165d55381
Jhosias007 <jhosias007@gmail.com>
2023-03-14 11:35:43 -0500

    Agrego la interaccion del icono de carrito de compras con el aside correspondiente, mejora: al abrir un menu, se cierra el otro en caso estar abierto

--- 05-CursoPracticoJS/forkProyecto/agregadoJS/main.js
+++ 05-CursoPracticoJS/forkProyecto/agregadoJS/main.js
@@ -1,15 +1,34 @@
-const menuEmail = document.querySelector(".navbar-right")
+const menuEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
 const menuHamIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
+const menuCartIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCartIcon.addEventListener("click", toggleCartAside);
 
 function toggleDesktopMenu() {
+    if (!aside.classList.contains("inactive")){
+        aside.classList.add("inactive");
+    }
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu () {
+    if (!aside.classList.contains("inactive")){
+        aside.classList.add("inactive");
+    }
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCartAside () {
+    if (!mobileMenu.classList.contains("inactive")) {
+        mobileMenu.classList.add("inactive");
+    }
+    if (!desktopMenu.classList.contains("inactive")){
+        desktopMenu.classList.add("inactive");
+    }
+    aside.classList.toggle("inactive");
 }
