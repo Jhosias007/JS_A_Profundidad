878ab778ae4ed77dd0b1d7eb858020a5b31e7cb7
Jhosias007 <jhosias007@gmail.com>
2023-03-15 16:02:46 -0500

    Area de circulo y su circunferencia

--- 06-TallerPracticoJSMatematica&EstadisticaBasica/I-FormulasBasicas/02-formulasMatematicasJS.js
+++ 06-TallerPracticoJSMatematica&EstadisticaBasica/I-FormulasBasicas/02-formulasMatematicasJS.js
@@ -39,3 +39,31 @@ console.log(calcTri(3, 5, 6, 7))
 console.log(calcTri(12, 6, 21, 7))
 
 console.groupEnd("Triangulo");
+
+console.group("Circulo");
+
+const rdCirculo = 3; // rd = radio
+const dmCirculo = rdCirculo * 2; // dm = diametro
+const PI = 3.1415;
+
+const circunferencia = dmCirculo * PI; // o tambien: 2*rdCirculo*PI
+const areaCirculo = (rdCirculo ** 2) * PI;
+
+console.log({
+    rdCirculo,
+    dmCirculo,
+    PI,
+    circunferencia,
+    areaCirculo
+});
+
+function calcCirculo (radio) {
+    return {
+        circunferencia: 2*radio*PI,
+        area: (Math.PI)*(Math.pow(radio, 2))
+    }
+}
+
+console.log(calcCirculo(3));
+
+console.groupEnd("Circulo");
