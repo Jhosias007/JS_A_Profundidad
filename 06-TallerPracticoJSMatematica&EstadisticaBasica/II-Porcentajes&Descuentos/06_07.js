1ed738c3a34b88039df33ad08c1931043fab3791
Jhosias007 <jhosias007@gmail.com>
2023-03-18 09:25:34 -0500

    Eplico descuentos con cupones y uso find y filter con func. flecha

--- 06-TallerPracticoJSMatematica&EstadisticaBasica/II-Porcentajes&Descuentos/06_07.js
+++ 06-TallerPracticoJSMatematica&EstadisticaBasica/II-Porcentajes&Descuentos/06_07.js
@@ -1,13 +1,50 @@
-const cantidad = document.querySelector("#cantidad");
-const porcentaje = document.querySelector("#porcentaje");
-const resultado = document.querySelector("#resultado");
-const btnResultado = document.querySelector("#btnResultado");
+const inputPrice = document.querySelector("#price");
+const inputCupon = document.querySelector("#cupon");
+const result = document.querySelector("#result");
+const btnCalcular = document.querySelector("#btnCalcular");
 
-btnResultado.addEventListener("click", calcular);
+const price = inputPrice.value;
+const cupon = inputCupon.value
 
-function calcular() {
-    resultado.innerHTML = cantidad.value * porcentaje.value / 100;
+const cupones = [];
+cupones.push({ // Agrego cupones al array de cupones
+    name: "cupon1",
+    descuento: 10,
+    limite: 200
+}, {
+    name: "cupon2",
+    descuento: 20,
+    limite: 200
+}, {
+    name: "cupon3",
+    descuento: 50,
+    limite: 200
+});
+
+btnCalcular.addEventListener("click", calculo)
+
+function calculo() {
+    const price = inputPrice.value;    
+    const cupon = inputCupon.value;    
+    let descuento;
+    if (!cupon || !price){ // Verifico que los datos esten
+        result.innerText = "Datos insuficientes";
+        return;
+    }
+    // filter: array con todos los elementos que coincidan
+    // find: el primer elemento que coincida
+    const cuponValidado = cupones.filter(cuponElemento => cuponElemento.name == cupon);
+
+    if (cuponValidado) {
+        descuento = cuponValidado[0].descuento;
+    } else {
+        result.innerText = "Cupon no valido";
+        return;
+    }
+
+    
+    const resultFormula = (price * (100 - descuento)) / 100;
+    result.innerText = resultFormula;
+
+
 }
-
-
-
